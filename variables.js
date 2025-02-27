31b116b376bd55d396263aab7f59ca48e2c37cd5
Rizalyn05 <rizalyndonatodomitita@gmail.com>
2025-02-27 21:14:04 +0800

    Delete variables.js

--- variables.js
+++
@@ -1,9 +0,0 @@
-let name = 'Rizalyn';
-let whatDoYouWannaBecomeInYourLife = 'programmer';
-let gender = 'Female';
-let twitterHandle = 'Rayzliin';
-
-console.log(name);
-console.log(whatDoYouWannaBecomeInYourLife);
-console.log(gender);
-console.log(twitterHandle);
